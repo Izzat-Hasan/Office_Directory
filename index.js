beb86eada8bc5f3917ba229330077a688e809b38
katherynmk <katherynmknutson@lewisu.edu>
2022-04-06 21:40:50 -0500

    oopsies

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 app = express()
 
 var url = require('url');
-var dt = require('./date-time');
+
 
 const port = process.env.PORT || 3000
 const majorVersion = 1
@@ -23,7 +23,7 @@ app.use(express.static(__dirname + '/static'))
 
 // The app.get functions below are being processed in Node.js running on the server.
 // Implement a custom About page.
-app.get('/about', (request, response) => {
+app.get('/Klump', (request, response) => {
 	console.log('Calling "/about" on the Node.js server.')
 	response.type('text/plain')
 	response.send('About Node.js on Azure Template.')
