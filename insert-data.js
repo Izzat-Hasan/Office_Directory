7a3dce1df57b9fc885cdf2e09c97e9a5734e00bd
katherynmk <katherynmknutson@lewisu.edu>
2022-04-06 21:33:35 -0500

    all professors

--- insert-data.js
+++ insert-data.js
@@ -14,12 +14,20 @@ const client = new MongoClient(url);
          const db = client.db(dbName);
          // make collection
          const col = db.collection("professors");
-         // Construct documents                                                                                                                                                             
-
+         // Construct documents        
+         //template for documents V                                                                                                                                                     
+        // { name: "", hours: "", roomnumber: "", email: ""}
         const docs = [
-                { name: "Ray Klump", hours: "Appointment" },
+                { name: "Dr. Ray Klump", hours: "Appointment", roomnumber: "N/A", email: "N/A"},
                 { name: "Paul Kim", hours: "Monday, Wednesday, Friday from 12:00pm - 1:00pm" },
-                
+                { name: "Dr. Amanda Harsey", hours: "N/A", roomnumber: "N/A", email: "N/A"},
+                { name: "Tom Dupre", hours: "N/A", roomnumber: "N/A", email: "N/A"},
+                { name: "Gina Martinez", hours: "Tuesday, Thursday from 11:00am - 12:30pm", roomnumber: "AS-130L", email: "N/A"},
+                { name: "Dr. Piotr Szczurek", hours: "Appointment", roomnumber: "N/A", email: "N/A"},
+                { name: "Dr. Brittany Stephenson", hours: "Monday 2:00pm - 3:00pm, Tuesday 1:00pm - 2:00pm, Wednesday 2:00pm - 3:00pm and Appointment", roomnumber: "N/A", email: "N/A"},
+                { name: "Dr. Cynthia Howard", hours: "Tuesday, Thursday 10:00am - 12:00pm", roomnumber: "N/A", email: "N/A"},
+                { name: "Rami Khasawneh", hours: "Tuesday, Wednesday 4:00pm - 8:00pm", roomnumber: "N/A", email: "N/A"},
+                { name: "Mathias Plass", hours: "Monday 10:00am - 12:00pm + 1:00pm - 2:00pm, Tuesday 12:00pm - 4:00pm, Wednesday 10:00am - 12:00pm + 1:00pm - 2:00pm, Friday 10:00am - 12:00pm + 1:00pm - 2:00pm + 3:00pm - 4:00pm", roomnumber: "N/A", email: "N/A"}
               ];
 
 
