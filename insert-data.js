7391e18fcb30d04c5cc4a636eff99a3fa5e951ed
katherynmk <katherynmknutson@lewisu.edu>
2022-04-06 21:18:25 -0500

    database improved

--- insert-data.js
+++ insert-data.js
@@ -12,22 +12,24 @@ const client = new MongoClient(url);
          await client.connect();
          console.log("Connected correctly to server");
          const db = client.db(dbName);
-         // Use the collection "people"
-         const col = db.collection("people");
-         // Construct a document                                                                                                                                                              
-         let personDocument = {
-             "name": { "first": "Alan", "last": "Turing" },
-             "birth": new Date(1912, 5, 23), // May 23, 1912                                                                                                                                 
-             "death": new Date(1954, 5, 7),  // May 7, 1954                                                                                                                                  
-             "contribs": [ "Turing machine", "Turing test", "Turingery" ],
-             "views": 1250000
-         }
-         // Insert a single document, wait for promise so we can read it back
-         const p = await col.insertOne(personDocument);
-         // Find one document
-         const myDoc = await col.findOne();
-         // Print to the console
-         console.log(myDoc);
+         // make collection
+         const col = db.collection("professors");
+         // Construct documents                                                                                                                                                             
+
+        const docs = [
+                { name: "Ray Klump", hours: "Appointment" },
+                { name: "Paul Kim", hours: "Monday, Wednesday, Friday from 12:00pm - 1:00pm" },
+                
+              ];
+
+
+        // this option prevents additional documents from being inserted if one fails
+        const options = { ordered: true };
+        const result = await col.insertMany(docs, options);
+        console.log(`${result.insertedCount} documents were inserted`);
+
+         console.log(docs);
+        
         } catch (err) {
          console.log(err.stack);
      }
