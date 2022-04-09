8c97d819a0a37759bac0e521663880032e66041c
Michael Arrigo <95397939+MikeTheStrat@users.noreply.github.com>
2022-04-09 17:03:01 -0500

    Updated info
    went thru entire cpsc online catalog and updates + added proffessor info, we need so send some emails to the new ones

--- insert-data.js
+++ insert-data.js
@@ -19,15 +19,26 @@ const client = new MongoClient(url);
         // { name: "", hours: "", roomnumber: "", email: ""}
         const docs = [
                 { name: "Dr. Ray Klump", hours: "Appointment", roomnumber: "N/A", email: "klumpra@lewisu.edu"},
-                { name: "Paul Kim", hours: "Monday, Wednesday, Friday from 12:00pm - 1:00pm", roomnumber: "N/A", email:"kimyo@lewisu.edu"},
-                { name: "Dr. Amanda Harsey", hours: "N/A", roomnumber: "N/A", email: "N/A"},
-                { name: "Tom Dupre", hours: "N/A", roomnumber: "N/A", email: "N/A"},
-                { name: "Gina Martinez", hours: "Tuesday, Thursday from 11:00am - 12:30pm", roomnumber: "AS-130L", email: "N/A"},
-                { name: "Dr. Piotr Szczurek", hours: "Appointment", roomnumber: "N/A", email: "N/A"},
-                { name: "Dr. Brittany Stephenson", hours: "Monday 2:00pm - 3:00pm, Tuesday 1:00pm - 2:00pm, Wednesday 2:00pm - 3:00pm and Appointment", roomnumber: "N/A", email: "N/A"},
-                { name: "Dr. Cynthia Howard", hours: "Tuesday, Thursday 10:00am - 12:00pm", roomnumber: "N/A", email: "N/A"},
-                { name: "Rami Khasawneh", hours: "Tuesday, Wednesday 4:00pm - 8:00pm", roomnumber: "N/A", email: "N/A"},
-                { name: "Mathias Plass", hours: "Monday 10:00am - 12:00pm + 1:00pm - 2:00pm, Tuesday 12:00pm - 4:00pm, Wednesday 10:00am - 12:00pm + 1:00pm - 2:00pm, Friday 10:00am - 12:00pm + 1:00pm - 2:00pm + 3:00pm - 4:00pm", roomnumber: "N/A", email: "N/A"}
+                { name: "Dr. Paul Kim", hours: "Monday, Wednesday, Friday from 12:00pm - 1:00pm", roomnumber: "N/A", email:"kimyo@lewisu.edu"},
+                { name: "Dr. Amanda Harsy", hours: "N/A", roomnumber: "N/A", email: "harsyram@lewisu.edu"},
+                { name: "Br. Tom Dupre", hours: "N/A", roomnumber: "N/A", email: "dupreth@lewisu.edu"},
+                { name: "Dr. Gina Martinez", hours: "Tuesday, Thursday from 11:00am - 12:30pm", roomnumber: "AS-130L", email: "martingi@lewisu.edu"},
+                { name: "Dr. Piotr Szczurek", hours: "Appointment", roomnumber: "N/A", email: "szczurpi@lewisu.edu"},
+                { name: "Dr. Brittany Stephenson", hours: "Monday 2:00pm - 3:00pm, Tuesday 1:00pm - 2:00pm, Wednesday 2:00pm - 3:00pm and Appointment", roomnumber: "N/A", email: "bstephenson@lewisu.edu"},
+                { name: "Dr. Cynthia Howard", hours: "Tuesday, Thursday 10:00am - 12:00pm", roomnumber: "N/A", email: "howardcy@lewisu.edu"},
+                { name: "Dr. Rami Khasawneh", hours: "Tuesday, Wednesday 4:00pm - 8:00pm", roomnumber: "N/A", email: "khasawra@lewisu.edu"},
+                { name: "Dr. Mathias Plass", hours: "Monday 10:00am - 12:00pm + 1:00pm - 2:00pm, Tuesday 12:00pm - 4:00pm, Wednesday 10:00am - 12:00pm + 1:00pm - 2:00pm, Friday 10:00am - 12:00pm + 1:00pm - 2:00pm + 3:00pm - 4:00pm", roomnumber: "N/A", email: "plassma@lewisu.edu"},
+                { name: "Dr. Sam Aboumar", hours: "N/A", roomnumber: "N/A", email: "oabuomar@lewisu.edu"},
+                { name: "Dr. Khaled Alzoubi", hours: "N/A", roomnumber: "N/A", email: "alzoubkh@lewisu.edu"},
+                { name: "Dr. Mahmood Al-khassaweneh", hours: "N/A", roomnumber: "N/A", email: "malkhassaweneh@lewisu.edu"},
+                { name: "Dr. Dana Dominiak", hours: "N/A", roomnumber: "N/A", email: "dominida@lewisu.edu"},
+                { name: "Margaret Juraco", hours: "N/A", roomnumber: "N/A", email: "juracoma@lewisu.edu"},
+                { name: "Dr. Michael Lewis", hours: "N/A", roomnumber: "N/A", email: "mlewis8@lewisu.edu"},
+                { name: "Dr. Marie Meyer", hours: "N/A", roomnumber: "N/A", email: "mmeyer2@lewisu.edu"},
+                { name: "Dr. Lucien Ngalamou", hours: "N/A", roomnumber: "N/A", email: "ngalamlu@lewisu.edu"},
+                { name: "Dr. Safwan Omari", hours: "N/A", roomnumber: "N/A", email: "omarisa@lewisu.edu"},
+                { name: "Dr. Jason Perry", hours: "N/A", roomnumber: "N/A", email: "perryjn@lewisu.edu"},
+                { name: "Eric Pogue", hours: "N/A", roomnumber: "N/A", email: "epogue@lewisu.edu"}
               ];
 
 
