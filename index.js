f4e33a2f44d7e4651a6a1c4b4f539a417b02c15a
katherynmk <katherynmknutson@lewisu.edu>
2022-04-20 18:07:22 -0500

    SORTA WORKING

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 app = express()
-
+const ObjectId = require('mongodb').ObjectId; 
+const { MongoClient, ServerApiVersion } = require('mongodb');
 var url = require('url');
 
 
@@ -8,24 +9,41 @@ const port = process.env.PORT || 3000
 const majorVersion = 1
 const minorVersion = 2
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = "mongodb+srv://lewisTeam:lewis123@information.puksi.mongodb.net/OfficeDirectory?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
   const collection = client.db("OfficeDirectory").collection("professors");
-  // perform actions on the collection object
+  async function run() {
+    try {
+         await client.connect();
+         console.log("Connected correctly to server");
+         const db = client.db("OfficeDirectory");
+         var query = {name: "Dr. Ray Klump" };
+         
+        var x =  collection.find(query)
+        console.log(x);
+  
+    } catch (err) {
+     console.log(err.stack);
+    }
+
+  finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
   console.log("databse working");
-  client.close();
+  //client.close();
 });
+
+
 
 // Use Express to publish static HTML, CSS, and JavaScript files that run in the browser. 
 app.use(express.static(__dirname + '/static'))
 
 // The app.get functions below are being processed in Node.js running on the server.
 // Implement a custom About page.
-
-
-
 
 
 
