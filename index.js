58e9f2cca9ede424a5847c3cde88534913065b06
katherynmk <katherynmknutson@lewisu.edu>
2022-04-21 14:33:26 -0500

    isolation

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const app = express()
 const mongoose = require('mongoose');
 const ejs = require('ejs');
 const { kStringMaxLength } = require('buffer');
+const { name } = require('ejs');
 
 
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb+srv://lewisTeam:lewis123@information.puksi.mongodb.net/OfficeDirectory?retryWrites=true&w=majority';
+const client = new MongoClient(url);
+const dbName = "OfficeDirectory";
+const db = client.db(dbName);
+const col = db.collection("professors");
 
 const port = process.env.PORT || 3000
 const majorVersion = 1
@@ -35,11 +42,9 @@ app.get('/views/rayklump', (req, res) => {
 })
 
 app.get('/views/kim', (req, res) => {
-  Professors.find({}, function(err, professors) {
-      res.render('kim', {
-          professorsList: professors
-      })
-  })
+    var query = { name: "Dr. Ray Klump"};
+    Professors.findOne(query).then(result => res.send(result)).catch(err => console.log(err))
+   
 })
 
 
