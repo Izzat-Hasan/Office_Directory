3e6bdd985231945d6c4177b3a5d7cceb3556f953
Katie Knutson <71470339+katherynmk@users.noreply.github.com>
2022-04-21 10:17:03 -0500

    cleaned up n sorta working

--- index.js
+++ index.js
@@ -1,49 +1,38 @@
 const express = require('express')
-app = express()
-const ObjectId = require('mongodb').ObjectId; 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-var url = require('url');
+const app = express()
+const mongoose = require('mongoose');
+const ejs = require('ejs');
+const { kStringMaxLength } = require('buffer');
+
+app.set('view engine', 'ejs');
 
 
 const port = process.env.PORT || 3000
 const majorVersion = 1
 const minorVersion = 2
 
-
-const uri = "mongodb+srv://lewisTeam:lewis123@information.puksi.mongodb.net/OfficeDirectory?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("OfficeDirectory").collection("professors");
-  async function run() {
-    try {
-         await client.connect();
-         console.log("Connected correctly to server");
-         const db = client.db("OfficeDirectory");
-         var query = {name: "Dr. Ray Klump" };
-         
-        var x =  collection.find(query)
-        console.log(x);
-  
-    } catch (err) {
-     console.log(err.stack);
-    }
-
-  finally {
-    await client.close();
-  }
-}
-run().catch(console.dir);
-  console.log("databse working");
-  //client.close();
-});
-
-
-
-// Use Express to publish static HTML, CSS, and JavaScript files that run in the browser. 
 app.use(express.static(__dirname + '/static'))
 
-// The app.get functions below are being processed in Node.js running on the server.
-// Implement a custom About page.
+mongoose.connect('mongodb+srv://lewisTeam:lewis123@information.puksi.mongodb.net/OfficeDirectory?retryWrites=true&w=majority');
+
+const professorSchema = {
+    name: String,
+    hours: String,
+    email: String,
+    phone: String,
+    roomnumber: String,
+    website: String
+}
+
+const Professors = mongoose.model('professors', professorSchema);
+
+app.get('/views', (req, res) => {
+    Professors.find({}, function(err, professors) {
+        res.render('rayklump', {
+            professorsList: professors
+        })
+    })
+})
 
 
 
